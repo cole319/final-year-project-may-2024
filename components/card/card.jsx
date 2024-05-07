cb5e9ff18e6736cfd262a96ecc17e150afa3f0ce
Suryarghya Saha <20je0998@cse.iitism.ac.in>
2024-05-07 20:29:21 +0530

    homepage done

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Image from "next/image";
 
-const card = () => {
-  return <div>card</div>;
+import Style from "../Card/card.module.css";
+import images from "../../assets";
+
+const Card = ({ candidateArray, giveVote }) => {
+  return (
+    <div className={Style.card}>
+      {candidateArray.map((el, i) => (
+        <div className={Style.card_box}>
+          <div className={Style.image}>
+            <img src={el[3]} alt="" />
+          </div>
+          <div className={Style.card_info}>
+            <h2>
+              {el[1]} #{el[2].toNumber()}
+            </h2>
+            <p>{el[0]}</p>
+            <p>Address: {el[6].slice(0, 10)}...</p>
+            <p className={Style.total}>Total Vote</p>
+          </div>
+          <div className={Style.card_vote}>
+            <p>{el[4].toNumber()}</p>
+          </div>
+          <div className={Style.card_button}>
+            <button
+              onClick={() => giveVote({ id: el[3].toNumber(), address: el[6] })}
+            >
+              Give Vote
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default card;
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,50 @@ import Countdown from "react-countdown";
 //Internal Import
 import { VotingContext } from "../context/Voter";
 import Style from "../styles/index.module.css";
-import Card from "../components/card/card";
+import Card from "../components/Card/Card";
 import image from "../assets/candidate-1.jpg";
 
 const index = () => {
-  const { votingTitle } = useContext(VotingContext);
-  return <div>{votingTitle}</div>;
+  const {
+    getNewCandidate,
+    candidateArray,
+    giveVote,
+    checkIfWalletIsConnected,
+    candidateLength,
+    voterLength,
+    currentAccount,
+  } = useContext(VotingContext);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  });
+
+  return (
+    <div className={Style.home}>
+      {currentAccount && (
+        <div className={Style.winner}>
+          <div className={Style.winner_info}>
+            <div className={Style.candidate_list}>
+              <p>
+                No Candidate: <span>{candidateLength}</span>
+              </p>
+            </div>
+            <div className={Style.candidate_list}>
+              <p>
+                No Voter: <span>{voterLength}</span>
+              </p>
+            </div>
+          </div>
+          <div className={Style.winner_message}>
+            <small>
+              <Countdown date={Date.now() + 10000} />
+            </small>
+          </div>
+        </div>
+      )}
+      <Card candidateArray={candidateArray} giveVote={giveVote} />
+    </div>
+  );
 };
 
 export default index;
